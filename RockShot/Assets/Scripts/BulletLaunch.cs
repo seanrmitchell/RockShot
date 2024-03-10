6dca3c847453c0d345d4e45247aca1d22b42348b
Sean Mitchell <seanrmitchell211@gmail.com>
2024-03-10 16:46:29 -0400

    feat. Gun Shot Sound
    Adds:
    - Gunshot Sound Effect
    
    Note:
    - Might want to change in future

--- RockShot/Assets/Scripts/BulletLaunch.cs
+++ RockShot/Assets/Scripts/BulletLaunch.cs
@@ -1,30 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class BulletLaunch : MonoBehaviour
 {
     public float bulletForce;
     public GameObject bulletPreFab;
     public Transform firePos;
+    public AudioSource aud;
 
     [SerializeField]
     private Vector3 shotHalfSize;
 
-    [SerializeField]
-    private float maxDistance;
-
-    private Ray ray;
-
     // Update is called once per frame
     void Update()
     {
-
+        
         // Casts ray from camera to a point correlated with mouse position
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            aud.Play();
             // Destroys rock if ray connects with it
             if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity) && hit.collider.gameObject.CompareTag("Rock"))
             {
